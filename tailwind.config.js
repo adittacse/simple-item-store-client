98ba29c0963afe88d27afff352efb3e558584574
Aditta Chakraborty <cseaditta@gmail.com>
2026-01-17 02:15:40 +0600

    bugs fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./app/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+    content: [
+        "./src/app/**/*.{js,jsx,ts,tsx,mdx}",
+        "./src/components/**/*.{js,jsx,ts,tsx,mdx}",
+        "./src/**/*.{js,jsx,ts,tsx,mdx}",
+    ],
     theme: { extend: {} },
     plugins: [require("daisyui")],
     daisyui: {
